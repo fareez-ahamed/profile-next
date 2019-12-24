2be472fd310c111a48a9437c7dd81bed3b754fdc
Fareez Ahamed <fareez.ahamed@gmail.com>
2019-12-24 19:27:51 +0530

    Bismillah

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home: React.FC<{}> = props => (
+  <div>
+    <Head>
+      <title>My styled page</title>
+      <link href="/styles.css" rel="stylesheet" />
+    </Head>
+    Bismillah
+  </div>
+);
+
+export default Home;
